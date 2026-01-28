8bbb2ee049ae6b9613ad4b76461f47d4c3c0ae4e
Mox90 <moctar.sandalo89@gmail.com>
2026-01-28 20:18:50 +0300

    Implemented Postcard

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FirstVideo from "./sections/FirstVideo.jsx";
 import Jason from "./sections/Jason.jsx";
 import SecondVideo from "./sections/SecondVideo.jsx";
 import Lucia from "./sections/Lucia.jsx";
+import PostCard from "./sections/PostCard.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -20,6 +21,8 @@ const App = () => {
 
             <SecondVideo />
             <Lucia />
+
+            <PostCard />
         </main>
     )
 }
